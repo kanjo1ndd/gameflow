518ab05ffe606c4c747e0fe9f9c53c6cb35630a6
kanjo1nd <bezlimitnyj568@gmail.com>
2025-09-30 19:54:24 +0300

    update game page

--- src/views/pageGame/PageGame.jsx
+++ src/views/pageGame/PageGame.jsx
@@ -16,7 +16,69 @@ export default function PageGame() {
                     ))}
                 </div>
                 <div className="category-game-page">
-                    
+                    <div className="text-category-game-page">
+                        екшн
+                    </div>
+                    <div className="text-category-game-page">
+                        рпг
+                    </div>
+                    <div className="text-category-game-page">
+                        <i className="bi bi-chevron-down"/>
+                    </div>
+                </div>
+                <div className="pre-description">
+                    Cyberpunk 2077 — пригодницький бойовик і рольова гра з відкритим світом. Дія відбувається у темному майбутньому Найт-Сіті, небезпечного мегаполіса, одержимого владою, гламуром і ненаситною модифікацією тіла.
+                </div>
+                <div className="chevron-down">
+                    <i className="bi bi-chevron-down"/>
+                </div>
+                <div className="name-game-page">Комплекти</div>
+                <div className="set-game-page">
+                    <div className="name-set-game-page">Name game</div>
+                    <div className="block-set-game-page">
+                        Cyberpunk 2077 — пригодницький рольовий екшн у відкритому світі мегаполісу Найт-Сіті, де у ролі кіберпанкового найманця ви боротиметеся за виживання. Гра вдосконалена і має новий безкоштовний вміст. Налаштуйте персонажа й ігровий стиль, виконуючи завдання, нарощуючи репутацію і відкриваючи апгрейди. Будуючи взаємини і здійснюючи вибір, ви формуєте сюжет і світ навколо. Тут народжуються легенди. Якою буде ваша?                  
+                        <div className="contents-text">Вміст:</div>
+                        <nav className="contents-set-game-page">
+                            <li>Cyperpunk</li>
+                            <li>Something</li>
+                            <li>Something</li>
+                            <li>Something</li>
+                        </nav>
+                    </div>
+                    <div className="price-button-game-page">
+                        <div className="price-game-page">9 999₴</div>
+                        <button className="button-basket">У кошик</button>
+                    </div>
+                </div>
+                <div className="set-game-page">
+                    <div className="name-set-game-page">Name game</div>
+                    <div className="block-set-game-page">
+                        <div className="contents-text">Вміст:</div>
+                        <nav className="contents-set-game-page">
+                            <li>Cyperpunk</li>
+                            <li>Something</li>
+                        </nav>
+                    </div>
+                    <div className="price-button-game-page">
+                        <div className="price-game-page">9 999₴</div>
+                        <button className="button-basket">У кошик</button>
+                    </div>
+                </div>
+                <div className="name-game-page">Інші DLC</div>
+                <div className="dlc-set-game-page">
+                    <div className="block-dlc-game-page">
+                        <div>Cyberpunk 2077 Bonus Content</div> <div>9 999₴</div>
+                    </div>
+                    <div className="block-dlc-game-page">
+                        <div>Cyberpunk 2077 REDmod</div> <div>9 999₴</div>
+                    </div>
+                    <div className="block-dlc-game-page">
+                        <div>Cyberpunk 2077: Ілюзія свободи</div> <div>9 999₴</div>
+                    </div>
+                    <div className="price-button-game-page">
+                        <div className="price-game-page">9 999₴</div>
+                        <button className="button-basket">Додати в кошик усі DLC</button>
+                    </div>
                 </div>
             </div>
             <div>
