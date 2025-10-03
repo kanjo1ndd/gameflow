5a5d2fffe9b73df4f6670cdec2c257c2b4d8d472
kanjo1nd <bezlimitnyj568@gmail.com>
2025-10-03 15:07:37 +0300

    finish charactreristics

--- src/views/pageGame/Characteristics.jsx
+++ src/views/pageGame/Characteristics.jsx
@@ -10,9 +10,76 @@ export default function Characteristics() {
         <div className="content-page-game">
             <HeaderPageGame />
             <div className="left-block-right-menu">
+                <div className="left-block-game-page">
+                    <div className="name-game-page">Name</div>
+                    <div className='block-platform'>
+                        <div className='text-platform'><i className='bi bi-windows'/>Windows</div> <i className='bi bi-chevron-down' />
+                    </div>
+                    <div className='blocks-characteristics'>
+                        <MinimalRequirements />
+                        <RecommendedRequirements />
+                    </div>
+                </div>
                 <MenuGamePage />
             </div>
         </div>
         <Footer />
+    </>;
+}
+
+export function MinimalRequirements() {
+
+    const requirements = [
+        { label: "Версія системи:", value: "64-bit Windows 10" },
+        { label: "CPU:", value: "Core i7-6700 or Ryzen 5 1600" },
+        { label: "Пам'ять:", value: "12 GB ОП" },
+        { label: "GPU:", value: "GeForce GTX 1060 6GB or Radeon RX 580 8GB or Arc A380" },
+        { label: "DirectX:", value: "версії 12" },
+        { label: "Обсяг пам'яті:", value: "70 GB" },
+        { label: "Звукова карта:", value: "Windows Compatible Audio Device" },
+        { label: "Контроллер:", value: "Клавіатура, геймпад" },
+        { label: "Додаткові примітки:", value: "Потрібен SSD" },
+        { label: "Мова:", value: "Англійська, Українська, Французька, Італійська, тощо" }
+    ];
+
+    return <>
+        <div className="block-requirements">
+            <div>Мінімальні налаштування</div>
+            <div className="blocks-requirements">
+                {requirements.map((req, index) => (
+                    <div className="text-requirements" key={index}>
+                        {req.label}
+                        <div className="text-requirement">{req.value}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </>;
+}
+
+export function RecommendedRequirements() {
+
+    const requirements = [
+        { label: "Версія системи:", value: "64-bit Windows 10" },
+        { label: "CPU:", value: "Core i7-6700 or Ryzen 5 1600" },
+        { label: "Пам'ять:", value: "12 GB ОП" },
+        { label: "GPU:", value: "GeForce GTX 1060 6GB or Radeon RX 580 8GB or Arc A380" },
+        { label: "DirectX:", value: "версії 12" },
+        { label: "Обсяг пам'яті:", value: "70 GB" },
+        { label: "Звукова карта:", value: "Windows Compatible Audio DeviceB" }
+    ];
+
+    return <>
+        <div className='block-requirements'>
+            <div className=''>Рекомендовані налаштування</div>
+            <div className="blocks-requirements">
+                {requirements.map((req, index) => (
+                    <div className="text-requirements" key={index}>
+                        {req.label}
+                        <div className="text-requirement">{req.value}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
     </>;
 }
